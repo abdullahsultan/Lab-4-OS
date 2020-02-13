4f1b83cec9165cdb966f597971a7384e45b43d9d
abdullahsultan <abdullahsultan448@gmail.com>
2020-02-13 23:53:13 +0500

    WokingFine

--- Task-1.c
+++ Task-1.c
@@ -1,2 +1,35 @@
 #include <unistd.h>
 #include <stdio.h>
+#include<sys/wait.h>
+#include<string.h>
+int main(int argc, char const *argv[]) {
+
+  int the_pipe[2];
+  int  pipeVal = pipe(the_pipe);
+  if (pipeVal == -1 ) {
+    printf("Pipe failed\n");
+  }
+
+  int forkVal =  fork();
+
+  if (forkVal > 0) {
+    //wait(NULL);
+    printf("In parent \n");
+    close(the_pipe[1]);
+    char recieved[100];
+    int rcvd = read(the_pipe[0],(char*)recieved,10);
+    printf("%d, Val of recieved %s \n",rcvd, recieved );
+  }
+  else if(forkVal == 0)
+  {
+    printf("In Child\n");
+    char input[10];
+    scanf("%s",input);
+    //strcpy(input,"test");
+    printf("%s\n",input);
+    close(the_pipe[0]);
+    int sent=write(the_pipe[1],(char*)input,10);
+    printf("Bytes sent %d\n",sent);
+  }
+  return 0;
+}
--- Task-2.c
+++ Task-2.c
@@ -1,32 +1,80 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include<sys/wait.h>
+
+int isPalindrome(char str[]);
 
 int main(int argc, char const *argv[]) {
 
-  int the_pipe_ofChild[2];
-  int  pipeVal = pipe(the_pipe_ofChild);
-  if (pipeVal == -1 ) {
-    printf("Pipe failed\n");
+  int the_pipe_1[2];
+  int the_pipe_2[2];
+
+  char recieved[20];
+  char input[100];
+  char pal[100];
+  int palchid;
+
+  int  pipeVal = pipe(the_pipe_1);
+  if (pipeVal < 0 ) {
+    printf("Pipe 1 failed\n");
+  }
+  if (pipe(the_pipe_2) == -1 ) {
+    printf("Pipe 2 failed\n");
   }
 
   int forkVal =  fork();
 
   if (forkVal > 0) {
-    wait(NULL);
     printf("In parent \n");
-    close(the_pipe_ofChild[1]);
-    char recieved;
-    read(the_pipe_ofChild[0],recieved,10);
-    printf("Val of recieved %c \n",&recieved );
+    close(the_pipe_1[0]);
+    printf("Enter the value\n");
+    gets(input);
+    writte(the_pipe_1[1], (char*) input,20);
+
+    wait(NULL);
+    close(the_pipe_2[1]);
+    read(the_pipe_2[0],pal,2);
+    if(pal == 1)
+    {
+      printf("Not plaindrome\n");
+    }
+    else
+      printf("Plaindrome\n");
+
   }
+
+
   else if(forkVal == 0)
   {
     printf("In Child\n");
-    char input;
-    scanf("%c",&input );
-    close(the_pipe_ofChild[0]);
-    write(the_pipe_ofChild[1],(char) input,10);
+    close(the_pipe_1[1]);
+
+    read(the_pipe_1[0],recieved,20);
+    printf("DUCKKKKKK\n");
+
+    printf("Val of recieved %s \n",&recieved );
+
+    palchid = isPalindrome(recieved);
+    close(the_pipe_2[0]);
+    write(the_pipe_2[1],palchid,2);
+
+
   }
   return 0;
+}
+
+int isPalindrome(char str[])
+{
+    int l = 0;
+    int h = strlen(str) - 1;
+
+    while (h > l)
+    {
+        if (str[l++] != str[h--])
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
